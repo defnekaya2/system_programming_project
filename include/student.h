426b9f934b1e2e11444bd1cf3d80d71e449aa59e
Nehir Okutur <nehir.okutur@stu.fbu.edu.tr>
2025-12-19 18:00:18 +0000

    feat: add main.cpp, student.h, CMakeLists.txt templates (Week 1)

---
+++ include/student.h
@@ -0,0 +1,9 @@
+#pragma once
+
+struct Student {
+    int id;
+    std::string name;
+    std::string surname;
+    std::string department;
+    std::string email;
+};
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Student Information System Week 1 - C++ setup" << std::endl;
+    return 0;
+}
